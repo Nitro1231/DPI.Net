856998000570db4b26dbe07961d6e5bbd9098135
nitro1231 <nitro0@naver.com>
2020-04-26 16:23:47 +0900

    Upload

---
+++ DPI/DPI/Main.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace DPI {
+    partial class Main {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.backColorP1 = new System.Windows.Forms.Panel();
+            this.backColorP2 = new System.Windows.Forms.Panel();
+            this.backColorP3 = new System.Windows.Forms.Panel();
+            this.backColorP4 = new System.Windows.Forms.Panel();
+            this.backColorP5 = new System.Windows.Forms.Panel();
+            this.backColorP6 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.title = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.langBox = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dpiBox = new System.Windows.Forms.ComboBox();
+            this.backupBox = new System.Windows.Forms.CheckBox();
+            this.apply = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // backColorP1
+            // 
+            this.backColorP1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(196)))), ((int)(((byte)(15)))));
+            this.backColorP1.Location = new System.Drawing.Point(0, 0);
+            this.backColorP1.Name = "backColorP1";
+            this.backColorP1.Size = new System.Drawing.Size(266, 5);
+            this.backColorP1.TabIndex = 0;
+            // 
+            // backColorP2
+            // 
+            this.backColorP2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(196)))), ((int)(((byte)(15)))));
+            this.backColorP2.Location = new System.Drawing.Point(0, 0);
+            this.backColorP2.Name = "backColorP2";
+            this.backColorP2.Size = new System.Drawing.Size(5, 200);
+            this.backColorP2.TabIndex = 1;
+            // 
+            // backColorP3
+            // 
+            this.backColorP3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(196)))), ((int)(((byte)(15)))));
+            this.backColorP3.Location = new System.Drawing.Point(0, 195);
+            this.backColorP3.Name = "backColorP3";
+            this.backColorP3.Size = new System.Drawing.Size(133, 5);
+            this.backColorP3.TabIndex = 2;
+            // 
+            // backColorP4
+            // 
+            this.backColorP4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(204)))), ((int)(((byte)(113)))));
+            this.backColorP4.Location = new System.Drawing.Point(133, 195);
+            this.backColorP4.Name = "backColorP4";
+            this.backColorP4.Size = new System.Drawing.Size(266, 5);
+            this.backColorP4.TabIndex = 3;
+            // 
+            // backColorP5
+            // 
+            this.backColorP5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(204)))), ((int)(((byte)(113)))));
+            this.backColorP5.Location = new System.Drawing.Point(395, 0);
+            this.backColorP5.Name = "backColorP5";
+            this.backColorP5.Size = new System.Drawing.Size(5, 200);
+            this.backColorP5.TabIndex = 4;
+            // 
+            // backColorP6
+            // 
+            this.backColorP6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(204)))), ((int)(((byte)(113)))));
+            this.backColorP6.Location = new System.Drawing.Point(266, 0);
+            this.backColorP6.Name = "backColorP6";
+            this.backColorP6.Size = new System.Drawing.Size(134, 5);
+            this.backColorP6.TabIndex = 4;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(76)))), ((int)(((byte)(60)))));
+            this.panel1.Location = new System.Drawing.Point(369, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(20, 20);
+            this.panel1.TabIndex = 5;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(196)))), ((int)(((byte)(15)))));
+            this.panel2.Location = new System.Drawing.Point(343, 11);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(20, 20);
+            this.panel2.TabIndex = 6;
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.Location = new System.Drawing.Point(12, 11);
+            this.title.Margin = new System.Windows.Forms.Padding(3);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(261, 17);
+            this.title.TabIndex = 7;
+            this.title.Text = "Auto Scale Mode Changer for Visual Studio";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 15);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Language:";
+            // 
+            // langBox
+            // 
+            this.langBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.langBox.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.langBox.ForeColor = System.Drawing.Color.White;
+            this.langBox.FormattingEnabled = true;
+            this.langBox.Location = new System.Drawing.Point(33, 71);
+            this.langBox.Name = "langBox";
+            this.langBox.Size = new System.Drawing.Size(144, 23);
+            this.langBox.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(210, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 15);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "DPI Mode: ";
+            // 
+            // dpiBox
+            // 
+            this.dpiBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.dpiBox.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.dpiBox.ForeColor = System.Drawing.Color.White;
+            this.dpiBox.FormattingEnabled = true;
+            this.dpiBox.Location = new System.Drawing.Point(213, 71);
+            this.dpiBox.Name = "dpiBox";
+            this.dpiBox.Size = new System.Drawing.Size(144, 23);
+            this.dpiBox.TabIndex = 12;
+            // 
+            // backupBox
+            // 
+            this.backupBox.AutoSize = true;
+            this.backupBox.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.backupBox.ForeColor = System.Drawing.Color.White;
+            this.backupBox.Location = new System.Drawing.Point(33, 100);
+            this.backupBox.Name = "backupBox";
+            this.backupBox.Size = new System.Drawing.Size(103, 19);
+            this.backupBox.TabIndex = 13;
+            this.backupBox.Text = "Make a Backup";
+            this.backupBox.UseVisualStyleBackColor = true;
+            // 
+            // apply
+            // 
+            this.apply.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.apply.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.apply.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(204)))), ((int)(((byte)(113)))));
+            this.apply.Location = new System.Drawing.Point(33, 125);
+            this.apply.Name = "apply";
+            this.apply.Size = new System.Drawing.Size(330, 38);
+            this.apply.TabIndex = 14;
+            this.apply.Text = "Apply";
+            this.apply.UseVisualStyleBackColor = true;
+            this.apply.Click += new System.EventHandler(this.apply_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.ClientSize = new System.Drawing.Size(400, 200);
+            this.Controls.Add(this.apply);
+            this.Controls.Add(this.backupBox);
+            this.Controls.Add(this.dpiBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.langBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.title);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.backColorP6);
+            this.Controls.Add(this.backColorP5);
+            this.Controls.Add(this.backColorP4);
+            this.Controls.Add(this.backColorP3);
+            this.Controls.Add(this.backColorP2);
+            this.Controls.Add(this.backColorP1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Main";
+            this.Text = "Auto Scale Mode Changer for Visual Studio";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel backColorP1;
+        private System.Windows.Forms.Panel backColorP2;
+        private System.Windows.Forms.Panel backColorP3;
+        private System.Windows.Forms.Panel backColorP4;
+        private System.Windows.Forms.Panel backColorP5;
+        private System.Windows.Forms.Panel backColorP6;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox langBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox dpiBox;
+        private System.Windows.Forms.CheckBox backupBox;
+        private System.Windows.Forms.Button apply;
+    }
+}
+
---
+++ DPI/DPI/Main.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DPI {
+    public partial class Main : Form {
+        public Main() {
+            InitializeComponent();
+
+            // Supporting Languages
+            langBox.Items.Add("VB.Net");
+            langBox.Items.Add("C#");
+
+            //Options: Dpi, Font (Defolat), Inherit, None
+            dpiBox.Items.Add("Font");
+            dpiBox.Items.Add("Dpi");
+            dpiBox.Items.Add("Inherit");
+            dpiBox.Items.Add("None");
+        }
+
+        private void Main_Load(object sender, EventArgs e) {
+
+        }
+
+        private void apply_Click(object sender, EventArgs e) {
+
+        }
+    }
+}
